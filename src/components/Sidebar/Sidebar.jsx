c7ca8ba1581f44abb6a6cf89784c8296d9a0dea8
Amaresh Maharana <amareshcoder@gmail.com>
2025-04-05 22:59:40 +0530

    Implement main layout with sidebar and main components and give styles.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import './Sidebar.css'
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+
   return (
     <>
-        <div className='sidebar'>
-            
-        </div>
-    </>
-  )
-}
+      <div className="sidebar">
+        <div className="top">
+          <img onClick={() => setExtended(prev => !prev)} className="menu" src={assets.menu_icon} alt="" />
 
-export default Sidebar
+          <div className="new-chat">
+            <img src={assets.plus_icon} alt="" />
+            {extended ? <p>New Chat</p> : null}
+          </div>
+
+          {extended ? (
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              <div className="recent-chat">
+                <img src={assets.message_icon} alt="" />
+                <p>What is AI...</p>
+              </div>
+            </div>
+          ) : null}
+        </div>
+
+        <div className="bottom">
+          <div className="bottom-item recent-chat">
+            <img src={assets.question_icon} alt="" />
+            {extended ? <p>Help</p> : null}
+          </div>
+
+          <div className="bottom-item recent-chat">
+            <img src={assets.history_icon} alt="" />
+            {extended ? <p>Activity</p> : null}
+          </div>
+
+          <div className="bottom-item recent-chat">
+            <img src={assets.setting_icon} alt="" />
+            {extended ? <p>Settings</p> : null}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
